8e8cd798dbdc25db37df28ce89f77609a98026ba
Icaro Lessa <icaro.ld92@gmail.com>
2022-05-13 20:33:36 -0300

    dash

--- Cadastro Animais/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');                                 // exportando modulo express
-const app = express();                                              // iniciando aplicação express
-const port = 2000;                                                  // definindo porta de operação
-
-app.get('/', (req, res) => res.send(`Servidor em execução`))        // usando o express para enviar uma resposta
-app.listen(port, () => console.log(`Executando na porta ${port}`)); // ativando a porta 2000 para escuta
