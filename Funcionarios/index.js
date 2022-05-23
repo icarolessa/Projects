202b733559763bef4dd9b72c26552982f8575161
icarolessa <icaro.ld92@gmail.com>
2022-05-23 20:45:09 -0300

    update name router

--- Funcionarios/index.js
+++ Funcionarios/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const routeFuncionario = require('./routes/routes')
 const app = express()
 const port = 3000
+
+routeFuncionario(app)
 
 app.listen(port, () => {
     console.log('Server on')
