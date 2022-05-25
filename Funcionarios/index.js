ffe14499ea462971ab96b7dfad62ec827c5f0dc5
icarolessa <icaro.ld92@gmail.com>
2022-05-25 20:09:30 -0300

    instalação do body-parser para resolver problema da leitura da requisição

--- Funcionarios/index.js
+++ Funcionarios/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routeFuncionario = require('./routes/routes')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json())
 routeFuncionario(app)
 
 app.listen(port, () => {
