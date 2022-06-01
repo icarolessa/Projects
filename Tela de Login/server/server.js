a4da8b2e69929b923b57efe8165b2a2e7cadf7ea
icarolessa <icaro.ld92@gmail.com>
2022-06-01 19:13:06 -0300

    create listen

--- Tela de Login/server/server.js
+++ Tela de Login/server/server.js
@@ -1,6 +1,10 @@
-const express = require('expres')
+const express = require('express')
 const server = express()
-const bodyParser = express('body-parser') // verificar se vai precisar
+const bodyParser = require('body-parser') // verificar se vai precisar
 const port = 3000
 
 server.use(bodyParser.json())
+
+server.listen(port, () =>{
+    console.log(`Server in port ${port}`)
+})
