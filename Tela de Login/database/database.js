036085792b55fd210d4da26ac6a2c10ce70d6522
icarolessa <icaro.ld92@gmail.com>
2022-06-07 18:39:12 -0300

    insert path data base mongodb

--- Tela de Login/database/database.js
+++ Tela de Login/database/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb://localhost:27017/teleLogin')
+mongoose.connect('mongodb://localhost:27017/eletronico')
     .then(() => {
         console.log('MongoDB on-line');
     }).catch(erro => {
